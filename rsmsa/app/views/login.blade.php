7d5739537428950fca02bfa8d1318fbb1129ebda
Vincent Minde <vincentminde@gmail.com>
2015-03-19 11:33:49 +0300

    Added site

--- rsmsa/app/views/login.blade.php
+++ rsmsa/app/views/login.blade.php
@@ -45,9 +45,9 @@
 <div layout="row" style="padding-top: 200px" >
 
 
-  <div class="col-md-6" style="padding:100px;padding-top: 0px">
+  <div class="col-md-6" >
 
-<h4  style="color:black;font-weight: bold;line-height: 100%">Welcome to the Integrated Road Safety Management System(iRoad)</h4>
+	<!-- <h4  style="color:black;font-weight: bold;line-height: 100%">Welcome to the Integrated Road Safety Management System(iRoad)</h4>
        <ul>
 
        <li style="list-style-image: url({{asset('img/tick_pic.jpg')}}) ">Easy Accident Report</li>
@@ -58,12 +58,34 @@
        <li style="list-style-image: url({{asset('img/tick_pic.jpg')}}) ">Simple Offense Capturing </li>
        <li style="list-style-image: url({{asset('img/tick_pic.jpg')}}) ">Accomodation Of Other Systems Involving Road Safety. </li>
 
-       </ul>
+       </ul> -->
+       <img src="img/applications.png" width="800px"/>
 
   </div>
+<style>
+::-webkit-input-placeholder {
+   color: red;
+   opacity:  1;
+}
 
-    <div class="col-md-4" style="padding-right: 100px">
+:-moz-placeholder { /* Firefox 18- */
+   color: red;  
+   opacity:  1;
+}
+
+::-moz-placeholder {  /* Firefox 19+ */
+   color: red;  
+   opacity:  1;
+}
+
+:-ms-input-placeholder {  
+   color: red;
+   opacity:  1;  
+}
+</style>
+    <div class="col-md-4" style="padding-right: 100px;padding-top:50px">
  <form class="form-signin" action="{{ url('login') }}"  method="post">
+ <h3 class="form-signin-heading">Iroad(Integrated Road Safety Management System)</h3>
 <h6 class="form-signin-heading" style="color:black;font-weight: bold">Please Enter Your Credentials To Access The System</h6>
 
         @foreach($errors->all() as $error)
@@ -77,9 +99,9 @@
         @endif
                <div class="login-wrap">
 
-                    <input type="text" class="form-control" placeholder="Police Rank Number" autofocus name="rankNo" required style="margin-bottom: 15px;border: 0px;border-bottom: 1px solid">
+                    <input type="text" class="form-control" style="background-color:transparent;border: 2px solid grey;" placeholder="Police Rank Number" autofocus name="rankNo" required style="margin-bottom: 15px;border: 0px;border-bottom: 1px solid">
 
-                    <input type="password" class="form-control" placeholder="Password" name="password" required style="border: 0px;border-bottom: 1px solid">
+                    <input type="password" class="form-control"  style="background-color:transparent;margin-top:10px;border: 2px solid grey;" placeholder="Password" name="password" required style="border: 0px;border-bottom: 1px solid">
 
                     <label class="checkbox">
                         <input type="checkbox" value="remember-me"> Remember me
@@ -89,7 +111,7 @@
                         </span>
                     </label>
 
-                    <button class="btn btn-primary btn-block"  name="submit" type="submit" ng-disabled="login_form.$invalid">Sign in</button>
+                    <button class="btn btn-primary btn-block"  style="  background-color: #4C5861;"name="submit" type="submit" ng-disabled="login_form.$invalid">Sign in</button>
 
         </div>
 </form>
